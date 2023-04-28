03ad8a4c437816cf085509586197401cb256a1ae
ckaytev <ckaytev@github.com>
2023-04-28 11:01:47 +1000

    faster-whisper instead openai-whisper

--- tgisper/tgisperbot.py
+++ tgisper/tgisperbot.py
@@ -4,13 +4,13 @@ from typing import BinaryIO
 import ffmpeg
 import numpy as np
 import telebot
-import whisper
+from faster_whisper import WhisperModel
 
 bot_id = os.getenv("BOT_ID")
 model_name = os.getenv("ASR_MODEL", "small")
 
 bot = telebot.TeleBot(bot_id)
-model = whisper.load_model(model_name)
+model = WhisperModel(model_name, device="cpu", compute_type="float32")
 SAMPLE_RATE = 16000
 
 
@@ -30,12 +30,13 @@ def get_voice_message(message):
     voice_meta = bot.get_file(message.voice.file_id)
     voice_bytes = bot.download_file(voice_meta.file_path)
     voice_audio = load_audio(voice_bytes)
-    transcribe_result = model.transcribe(
+    segments, _ = model.transcribe(
         audio=voice_audio,
-        task="transcribe",
-        language="ru",
+        vad_filter=True,
+        beam_size=1,
     )
-    bot.send_message(message.from_user.id, transcribe_result["text"])
+    text = ' '.join([segment.text for segment in segments])
+    bot.send_message(message.from_user.id, text)
 
 
 def load_audio(binary_file: BinaryIO, sr: int = SAMPLE_RATE):
@@ -71,4 +72,4 @@ def load_audio(binary_file: BinaryIO, sr: int = SAMPLE_RATE):
 
 def main():
     print("Start polling")
-    bot.infinity_polling(timeout=10, long_polling_timeout = 5)
+    bot.infinity_polling(timeout=10, long_polling_timeout=5)
